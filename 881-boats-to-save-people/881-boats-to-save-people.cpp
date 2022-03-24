29573dec873e2d665c80815267bc6271c6103cb5
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-03-24 20:16:28 +0530

    Time: 109 ms (50.91%), Space: 42 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -3,26 +3,24 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
         int n=people.size();
-        vector<int>vis(n,0);
+        // vector<int>vis(n,0);
         int count=0;
         int l=0;
         int h=n-1;
         while(h>l){
             if(people[h]+people[l]<=limit){
-                vis[l]=1;
-                vis[h]=1;
+                people[h] = -1;
+                people[l] = -1;
                 h--;
                 l++;
                 count++;
             }
             else{
-                count++;
-                vis[h]=1;
                 h--;
             }
         }
-        for(int i=0;i<vis.size();i++){
-            if(vis[i]!=1)
+        for(int i=0;i<people.size();i++){
+            if(people[i]!=-1)
                 count++;
         }
         return count;
