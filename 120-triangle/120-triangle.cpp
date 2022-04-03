3dda5ed01ad5540554a3ddcc40b1c5a76967f91c
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-04-04 01:55:58 +0530

    Time: 8 ms (62.92%), Space: 9.1 MB (9.24%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,11 @@
 class Solution {
     int minsum(int i,int j,int n,int m,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-        //if(i==n-1)return
-        // cout<<i<<j<<endl;
-        if(i>n-1)
-            return 0;
+        //if(i==n-1)return triangle[i][j];
+        if(i>n-1)return 0;
         
         if(dp[i][j]!=-1)return dp[i][j];
         else{
-            int left=minsum(i+1,j,n,m,triangle,dp);
-            int right=minsum(i+1,j+1,n,m,triangle,dp);
-            int sum=min(left,right);
-            sum+=triangle[i][j];
-            return dp[i][j]=sum;
-         //return dp[i][j]=triangle[i][j]+min(,);
+         return dp[i][j]=triangle[i][j]+min(minsum(i+1,j,n,m,triangle,dp),minsum(i+1,j+1,n,m,triangle,dp));
     }        
         }
 public:
