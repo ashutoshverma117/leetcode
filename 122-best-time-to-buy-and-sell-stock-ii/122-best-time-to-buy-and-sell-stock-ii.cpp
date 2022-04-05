fc9049cb58b9d4d4fb86df7b927f9dcf42beaf14
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-04-05 21:01:55 +0530

    Time: 11 ms (40.12%), Space: 15.1 MB (7.71%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,9 +2,8 @@ class Solution {
     int count(int n,vector<int>&prices,vector<int>&dp){
         if(n==0)return 0;
         if(dp[n]!=-1)return dp[n];
-        else{
         return dp[n]=max(count(n-1,prices,dp)+prices[n]-prices[n-1],count(n-1,prices,dp));
-        }
+        
     }
 public:
     int maxProfit(vector<int>& prices) {
