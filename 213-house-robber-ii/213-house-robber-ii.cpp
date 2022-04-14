9d4ecb5df423c1293a61a253cb46fa927d0dab74
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-04-14 23:58:46 +0530

    Time: 7 ms (12.31%), Space: 11.4 MB (5.00%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
     int count(int n,vector<int>nums,vector<int>&dp){
         if(n<0)return 0;
         if(dp[n]!=-1)return dp[n];
-        else{
-        return dp[n]=max(nums[n]+count(n-2,nums,dp),count(n-1,nums,dp));
-        }
+        
+        return dp[n]= max(count(n-2,nums,dp)+nums[n],count(n-1,nums,dp));
     }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)
+          if(n==1)
             return nums[0];
-        vector<int>v,k;
-        vector<int>dp(n+1,-1);
-         vector<int>dpi(n+1,-1);
+        vector<int>v;
+        vector<int>k;
+        vector<int>dp(n,-1);
+        vector<int>dpi(n,-1);
         for(int i=0;i<n;i++){
             if(i!=0)v.push_back(nums[i]);
             if(i!=n-1)k.push_back(nums[i]);
