3d359cbbdce71b6c01fd158567e3a0d3f533b31e
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-05-04 01:03:31 +0530

    Time: 21 ms (19.13%), Space: 7.3 MB (78.65%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<int,int>mappi;
+        for(int i=0;i<s.size();i++){
+            mappi[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            if(mappi.find(t[i])==mappi.end())
+                mappi[t[i]]++;
+            else
+            mappi[t[i]]--;
+        }
+        for(auto x:mappi){
+            if(x.second>0)
+                return false;
+        }return true;
+    }
+};
