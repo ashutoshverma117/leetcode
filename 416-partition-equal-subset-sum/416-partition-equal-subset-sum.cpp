2dc08740fcd5e04c36df70107643fd04657d0fcd
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-07-12 01:41:05 +0530

    Time: 37 ms (95.66%), Space: 76 MB (13.32%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,39 +1,23 @@
 class Solution {
-    bool sum(int n,int half,vector<int>&nums,vector<vector<int>>&dp){
-        if(half==0)return 1;
-        //if(n==0) {
-            // if(half-nums[n]==0)return true;
-            // else
-            //     return false;
-        //}
-        if(n<0 || half<0)return false;
-        if(dp[n][half]!=-1)
-            return dp[n][half];
-        bool a=sum(n-1,half-nums[n],nums,dp);
-        bool b=sum(n-1,half,nums,dp);
-        bool ans=a||b;
-        if(ans){
-            dp[n][half]=1;
-            return true;
-        }
-        else{
-            dp[n][half]=0;
-            return false;
-        }
-    }
 public:
+    bool solve(int n,vector<int>& nums,int sum,vector<vector<int>>&dp){
+        if(n<0 || sum<0)
+            return false;
+        if(sum==0)
+            return true;
+        if(dp[n][sum]!=-1)return dp[n][sum];
+        return dp[n][sum]=solve(n-1,nums,sum-nums[n],dp)||solve(n-1,nums,sum,dp);
+    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        int sumi=0;
+        int sum=0;
         for(int i=0;i<n;i++){
-            sumi+=nums[i];
+            sum+=nums[i];
         }
-        if(sumi%2!=0)
+        if(sum%2!=0)
             return false;
-        else{
-        vector<vector<int>>dp(n+1,vector<int>(sumi/2+1,-1));
-        return sum(n-1,sumi/2,nums,dp);
-        }
+        vector<vector<int>>dp(n+1,vector<int>(sum/2+1,-1));
+        return solve(n-1,nums,sum/2,dp);
         
     }
 };
