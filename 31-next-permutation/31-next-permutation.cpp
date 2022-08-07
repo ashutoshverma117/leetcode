e9725a6638f80620768f747da646db667226f60d
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-08-07 20:15:27 +0530

    Time: 11 ms (26.16%), Space: 12.2 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
-        int i;
-        int j;
+        int cut=0;
+        int india=0;
+        int pak=0;
         int k=0;
         if(n==1)
-            return;
-        for(i=n-2;i>=0;i--){
+            return ;
+        for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
+                india=i;
+                cut=nums[i];
                 k=1;
                 break;
             }
@@ -17,12 +20,16 @@ public:
             reverse(nums.begin(),nums.end());
         }
         else{
-        for(j=n-1;j>i;j--){
-            if(nums[j]>nums[i])
+        for(int i=n-1;i>india;i--){
+            if(nums[i]>cut){
+                pak=i;
                 break;
+            }
         }
-                swap(nums[i],nums[j]);     
-        reverse(nums.begin()+i+1,nums.end());
+        
+        swap(nums[india],nums[pak]);
+        reverse(nums.begin()+india+1,nums.end());
+        
         }
     }
 };
